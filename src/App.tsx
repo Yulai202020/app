a011f6ad80e7ed90e4dba0e15f46a8c77d2e1b7e
Yulai202020 <ngmyulai@gmail.com>
2024-08-19 17:38:57 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,22 @@ import About from './sites/About';
 import Navigate from './sites/Navigate';
 import PageNotFound from './sites/PageNotFound';
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const basename = "/app";
 
 function App() {
   return (
-    <Router basename={basename}>
-      <Navigate />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </Router>
+    <BrowserRouter basename={basename}>
+      <div>
+        <Navigate />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
