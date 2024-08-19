f7a577f4225b55e12514a31158bfe8cae21ea26f
Yulai202020 <ngmyulai@gmail.com>
2024-08-19 17:35:13 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './sites/Home';
 import About from './sites/About';
 import PageNotFound from './sites/PageNotFound';
-import './index.css';
+
+import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const basename = "/app";
 
 function App() {
   return (
     <Router basename={basename}>
+      <nav>
+        <Link to="/">Home</Link> | 
+         <Link to="/about">About</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
